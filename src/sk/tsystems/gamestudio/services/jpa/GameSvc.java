64b59ef7a2daa3754d108fa9ea2e6e69babfdac8
DaliborAdamcik <diaceu@gmail.com>
2016-07-17 14:07:56 +0200

    b

--- src/sk/tsystems/gamestudio/services/jpa/GameSvc.java
+++ src/sk/tsystems/gamestudio/services/jpa/GameSvc.java
@@ -2,6 +2,10 @@ package sk.tsystems.gamestudio.services.jpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import sk.tsystems.gamestudio.entity.CommentEntity;
 import sk.tsystems.gamestudio.entity.GameEntity;
 import sk.tsystems.gamestudio.services.GameService;
 
@@ -13,14 +17,33 @@ public class GameSvc extends JpaConnector implements GameService {
 
 	@Override
 	public GameEntity getGame(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		try
+		{
+			EntityManager em = JpaConnector.getEntityManager();
+		JpaConnector.beginTransaction();
+
+		Query que = em.createQuery("SELECT c FROM CommentEntity c WHERE c.Game = ").setParameter("Game", game).setMaxResults(10);
+		
+			return (List<CommentEntity>) que.getResultList();
+		}
+		finally
+		{
+			JpaConnector.commitTransaction();
+		}
 	}
 
 	@Override
 	public List<GameEntity> listGames() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private void addgame(GameEntity game)
+	{
+		EntityManager em = JpaConnector.getEntityManager();
+		JpaConnector.beginTransaction();
+		em.persist(game);
+		JpaConnector.commitTransaction();
 	}
 
 }
